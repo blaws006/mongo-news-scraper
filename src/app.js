65c71ee3e7ee48a98fdf8529c85569451ba3cfd9
blaws006 <blaws006@gmail.com>
2019-04-03 18:44:21 -0400

    Handlebars and public files implemented

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
 // Core packages
+const path = require('path');
 
 // Dependencies
 const express = require('express');
 
-
 // Custom Imports
-
 
 const PORT = 3000;
 const app = express();
 
+const publicDirectoryPath = path.join(__dirname, '../public');
+
 app.set('view engine', 'hbs');
+app.use(express.static(publicDirectoryPath));
 
 app.get('/', (req, res) => {
   res.render('index')
